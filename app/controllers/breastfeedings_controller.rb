4c30c5850fe27ecfaf0994f971a94c8b00c7069f
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-29 01:05:30 -0300

    ADDED select to new breastfeeding

--- app/controllers/breastfeedings_controller.rb
+++ app/controllers/breastfeedings_controller.rb
@@ -9,5 +9,31 @@ class BreastfeedingsController < ApplicationController
         @babies = Baby.all
     end
 
+    def create
+        @breastfeeding = Breastfeeding.new(breastfeeding_params)
+
+        respond_to do |format|
+            if @breastfeeding.save
+                format.html { redirect_to @breastfeeding, notice: 'Tu registro de alimentacion a tu bebe ha sido guardado con exito' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    def show
+        @breastfeeding = Breastfeeding.find(params[:id])
+    end
+
+
+
+
+
+    private
+
+    def breastfeeding_params
+        params.require(:breastfeeding).permit(:date, :quantity, :length, :baby_id)
+    end
+
 
 end
