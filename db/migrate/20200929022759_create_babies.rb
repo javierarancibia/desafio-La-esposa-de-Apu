d77330933111ccb62d9e97e67ae549ce89387a27
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-29 20:06:10 -0300

    ADDED image url option to babies

--- db/migrate/20200929022759_create_babies.rb
+++ db/migrate/20200929022759_create_babies.rb
@@ -2,6 +2,7 @@ class CreateBabies < ActiveRecord::Migration[5.2]
   def change
     create_table :babies do |t|
       t.string :name
+      t.string :image
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_022845) do
 
   create_table "babies", force: :cascade do |t|
     t.string "name"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
